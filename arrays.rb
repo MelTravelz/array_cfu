a92a246c5774578617e8496e4ae96d8134d42d57
Melony Franchini <meltravelz@gmail.com>
2022-11-02 15:58:36 -0600

    Add index position information

--- arrays.rb
+++ arrays.rb
@@ -1,22 +1,44 @@
-ingredients = ["chocolate", "walnuts", "butter", "flour"]
+puts ingredients = ["chocolate", "walnuts", "butter", "flour"]
 
-min_cook_times = [20, 25, 35, 40]
+puts min_cook_times = [20, 25, 35, 40]
 
-prices = [3.99, 5.47, 15.06, 8.83]
+puts prices = [3.99, 5.47, 15.06, 8.83]
 
-purchases = [true, true, false, false]
+puts purchases = [true, true, false, false]
+
 
 
 # The 'pop' method will delete the last element (most recently added element) of an array. 
-ingredients.pop
+puts ingredients.pop
 
 # The 'shift' method will delete the first element of an array.
-min_cook_times.shift
+puts min_cook_times.shift
 
 # The "unshift" method will add an element to the beginning of an array.
-prices.unshift 5.95
+puts prices.unshift 5.95
 
 # The "push" method will add an element to the end (after the most recently added element) of an array.
 
-purchases.push false
+puts purchases.push false
+
+
+
+# Index positions usually start with 0 in coding. 
+# So if there are 5 elements in an array the index codes for those elements would be: 0, 1, 2, 3, 4
+# Let's see what element is in index position 2 of our ingredients array: 
+
+puts ingredients [3] 
+
+#I'm not sure how to show you the results, but I `cmd + /' all the other text besides line 1 and line 26`
+# I ran 'ruby arrays.rb' in terminal and the results were: 
+
+# chocolate
+# walnuts
+# butter
+# flour
+# flour
+
+# The last "flour" element is the answer to our index position question. 
+# "flour" is in index position 3 in the ingredients array. 
+
 
