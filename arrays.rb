7767343658ac02d796c961a49052a2b690cdb1a7
Melony Franchini <meltravelz@gmail.com>
2022-11-02 15:49:30 -0600

    Add array methods

--- arrays.rb
+++ arrays.rb
@@ -5,3 +5,18 @@ min_cook_times = [20, 25, 35, 40]
 prices = [3.99, 5.47, 15.06, 8.83]
 
 purchases = [true, true, false, false]
+
+
+# The 'pop' method will delete the last element (most recently added element) of an array. 
+ingredients.pop
+
+# The 'shift' method will delete the first element of an array.
+min_cook_times.shift
+
+# The "unshift" method will add an element to the beginning of an array.
+prices.unshift 5.95
+
+# The "push" method will add an element to the end (after the most recently added element) of an array.
+
+purchases.push false
+
