e1662369554cf84443897e6f1210877f69b62e55
Melony Franchini <meltravelz@gmail.com>
2022-11-02 18:18:26 -0600

    Correct Index Position Error

--- arrays.rb
+++ arrays.rb
@@ -1,12 +1,10 @@
-# puts ingredients = ["chocolate", "walnuts", "butter", "flour"]
+#puts ingredients = ["chocolate", "walnuts", "butter", "flour"]
 
 # puts min_cook_times = [20, 25, 35, 40]
 
 # puts prices = [3.99, 5.47, 15.06, 8.83]
 
 puts purchases = [true, true, false, false]
-
-
 
 # The 'pop' method will delete the last element (most recently added element) of an array. 
 # puts ingredients.pop
@@ -21,13 +19,11 @@ puts purchases = [true, true, false, false]
 
 # puts purchases.push false
 
-
-
 # # Index positions usually start with 0 in coding. 
 # # So if there are 5 elements in an array the index codes for those elements would be: 0, 1, 2, 3, 4
 # # Let's see what element is in index position 2 of our ingredients array: 
 
-# puts ingredients [3] 
+#puts ingredients [2] 
 
 #I'm not sure how to show you the results, but I `cmd + /' all the other text besides lines 1 and 26.
 # I ran 'ruby arrays.rb' in terminal and the results were: 
@@ -36,11 +32,11 @@ puts purchases = [true, true, false, false]
 # walnuts
 # butter
 # flour
-# flour
+# butter
 
-# The last "flour" element is the answer to our index position question. 
-# "flour" is in index position 3 in the ingredients array. 
-
+# The last element "butter" is the answer to what is in the index position 2. 
+# "butter" is in index position 2 in the ingredients array but it is the thrid element 
+# of our list since the array started with 0 for the first element.
 
 # Let's look at the clear method.
 # This methods empties (or clears out) an array. It leaves nothing in the array 
@@ -52,6 +48,6 @@ puts purchases.clear
 puts "Test new purchases array:"
 puts purchases
 
-# Again, I'm not sure how to show you this in Terminal, but I commented out all except lines 7 and 49.
+# Again, I'm not sure how to show you this in Terminal, but I commented out all except lines 7 and 44-48.
 # Then I ran `ruby arrays.rb` the original list of variables was listed, the two string comments and nothing else.
 # I suppose this worked because there was no other output for the purchases array. :) 
